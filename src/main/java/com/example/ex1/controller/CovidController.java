cbfd5d4f7943d0b681dee8f61cf3d05ac2141fa4
Jonathan Chen <sabza178@gmail.com>
2024-01-31 16:54:29 +0700

    adding findByProvince method

--- src/main/java/com/example/ex1/controller/CovidController.java
+++ src/main/java/com/example/ex1/controller/CovidController.java
@@ -5,9 +5,11 @@ import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -23,5 +25,24 @@ public class CovidController {
 
         return covidData;
 
+    }
+
+    @GetMapping("/covid/{province}")
+    public List<Covid> findByProvince(@PathVariable String province){
+        RestTemplate restTemplate = new RestTemplate();
+        String url = "https://covid19.ddc.moph.go.th/api/Cases/today-cases-by-provinces";
+
+        ParameterizedTypeReference<List<Covid>> responseType = new ParameterizedTypeReference<List<Covid>>() {};
+        ResponseEntity<List<Covid>> responseEntity = restTemplate.exchange(url, HttpMethod.GET, null, responseType);
+        List<Covid> covidData = responseEntity.getBody();
+
+        List<Covid> filteredData = new ArrayList<>();
+        for(Covid covid : covidData){
+            if(covid.getProvince().equalsIgnoreCase(province)){
+                return filteredData;
+            }
+        }
+
+        return filteredData;
     }
 }
