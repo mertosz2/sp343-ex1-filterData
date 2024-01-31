fbdf297a95d4d36966e04b0410ed90f4d608ca89
Jonathan Chen <sabza178@gmail.com>
2024-01-31 17:24:44 +0700

    add findByProvince2 using stream filter

--- src/main/java/com/example/ex1/controller/CovidController.java
+++ src/main/java/com/example/ex1/controller/CovidController.java
@@ -11,6 +11,7 @@ import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class CovidController {
@@ -39,10 +40,26 @@ public class CovidController {
         List<Covid> filteredData = new ArrayList<>();
         for(Covid covid : covidData){
             if(covid.getProvince().equalsIgnoreCase(province)){
-                return filteredData;
+                filteredData.add(covid);
             }
         }
 
         return filteredData;
+    }
+
+    //using stream
+    @GetMapping("/covid2/{province}")
+    public List<Covid> findByProvince2(@PathVariable String province){
+        RestTemplate restTemplate2 = new RestTemplate();
+        String url = "https://covid19.ddc.moph.go.th/api/Cases/today-cases-by-provinces";
+
+        ParameterizedTypeReference<List<Covid>> responseType2 = new ParameterizedTypeReference<List<Covid>>() {};
+        ResponseEntity<List<Covid>> responseEntity2 = restTemplate2.exchange(url, HttpMethod.GET, null, responseType2);
+        List<Covid> covidData2 = responseEntity2.getBody();
+
+        List<Covid> filteredData2 = covidData2.stream()
+                                            .filter(covid -> province.equalsIgnoreCase(covid.getProvince()))
+                                            .collect(Collectors.toList());
+        return filteredData2;
     }
 }
