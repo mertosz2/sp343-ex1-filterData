2587b8860307fc01979f86dcac9d920c9e48b3eb
Jonathan Chen <sabza178@gmail.com>
2024-01-31 16:48:51 +0700

    add testgetAll method

---
+++ src/main/java/com/example/ex1/controller/CovidController.java
@@ -0,0 +1,27 @@
+package com.example.ex1.controller;
+
+import com.example.ex1.model.Covid;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+@RestController
+public class CovidController {
+    @GetMapping("/getAll")
+    public List<Covid> test(){
+        RestTemplate restTemplate = new RestTemplate();
+        String url = "https://covid19.ddc.moph.go.th/api/Cases/today-cases-by-provinces";
+
+        ParameterizedTypeReference<List<Covid>> responseType = new ParameterizedTypeReference<List<Covid>>() {};
+        ResponseEntity<List<Covid>> responseEntity = restTemplate.exchange(url, HttpMethod.GET, null, responseType);
+        List<Covid> covidData = responseEntity.getBody();
+
+        return covidData;
+
+    }
+}
